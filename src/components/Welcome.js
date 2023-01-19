ae577f1f719421844868cd9987d3bd41a60416e0
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-19 13:47:34 +0530

    commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
-// write code for Welcome component here
+import React from "react";
+
+const Welcome = ({name}) => {
+    let sayHello = `Hello ${name}!`;
+    let msg = `Welcome to Newton School`;
+    return(
+        <div>
+            <h1>{sayHello}</h1>
+            <h2>{msg}</h2>
+        </div>
+    );
+};
+
+export default Welcome;
