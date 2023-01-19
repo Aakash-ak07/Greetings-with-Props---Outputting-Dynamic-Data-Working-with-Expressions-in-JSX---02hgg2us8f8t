25bbd9f129ec79d64e6e2013252dfaf5ba17943f
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-19 14:02:08 +0530

    commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Welcome = ({name}) => {
-    let sayHello = `Hello ${name}!`;
+    let sayHi = `Hey ${name}!`;
     let greetMessage = `Welcome to Newton School.`;
     return (
         <div>
-            <h1>{sayHello}</h1>
+            <h1>{sayHi}</h1>
             <h2>{greetMessage}</h2>
         </div>
     );
